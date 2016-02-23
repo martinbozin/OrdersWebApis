ff6d196ea75b5e679644143bdba721ea3a504591
martinbozin <martinbozin>
2016-02-23 20:54:33 +0100

    finished REST services

--- OrdersApp.ViewModels/CustomerViewModel.cs
+++ OrdersApp.ViewModels/CustomerViewModel.cs
@@ -1,9 +1,18 @@
-﻿namespace OrdersApp.ViewModels
+﻿using System.Collections.Generic;
+
+namespace OrdersApp.ViewModels
 {
     public class CustomerViewModel
     {
+        public List<OrderViewModel> Orders;
+        public CustomerViewModel()
+        {
+            Orders = new List<OrderViewModel>();
+        }
+
         public virtual int CustomerId { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+ 
     }
 }
--- OrdersApp.WebApi/Controllers/CustomersController.cs
+++ OrdersApp.WebApi/Controllers/CustomersController.cs
@@ -1,36 +1,129 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Http;
+using log4net;
+using OrdersApp.Data;
+using OrdersApp.Domain;
+using OrdersApp.ViewModels;
+
 
 namespace OrdersApp.WebApi.Controllers
 {
     [Authorize]
     public class CustomersController : ApiController
     {
-        // GET api/values
-        public IEnumerable<string> Get()
+        private static readonly ILog Log;
+
+        // GET  V1/Customers
+        public IQueryable<CustomerViewModel> GetCustomers()
         {
-            return new string[] { "value1", "value2" };
+            try
+            {
+                var orderAppDbContext = new OrdersAppDbContext();
+
+                var customers = from b in orderAppDbContext.Customer
+                            select new CustomerViewModel()
+                            {
+                                CustomerId = b.CustomerId,
+                                Email = b.Email,
+                                Name = b.Name
+                            };
+
+                return customers;
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex);
+                return null;
+            }
         }
 
-        // GET api/values/5
-        public string Get(int id)
+        // GET V1/Customers/{id}
+        public CustomerViewModel Get(int id)
         {
-            return "value";
+            try
+            {
+                var orderAppDbContext = new OrdersAppDbContext();
+
+                var customer = orderAppDbContext.Customer.FirstOrDefault(x => x.CustomerId == id);
+                if (customer != null)
+                {
+                    var returnCustomer = new CustomerViewModel
+                    {
+                        CustomerId = customer.CustomerId,
+                        Email = customer.Email,
+                        Name = customer.Name
+                    };
+                    if (customer.CustomerOrders.Any())
+                    {
+                        var listUserOrders = new List<OrderViewModel>();
+                        foreach (var item in customer.CustomerOrders)
+                        {
+                            var order = new OrderViewModel
+                            {
+                                OrderId = item.OrderId,
+                                Price = item.Price,
+                                CreatedDate = item.CreatedDate
+                            };
+                            listUserOrders.Add(order);
+                        }
+                        returnCustomer.Orders = listUserOrders;
+                    }
+                    return returnCustomer;
+                }
+                return null;
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex);
+                return null;
+            }
         }
 
-        // POST api/values
-        public void Post([FromBody]string value)
+        // POST V1/Customers/{id} 
+        public Order Post(int id, OrderViewModel model)
         {
+            try
+            {
+                var orderAppDbContext = new OrdersAppDbContext();
+
+                var customer = orderAppDbContext.Customer.FirstOrDefault(x => x.CustomerId == id);
+                if (customer != null)
+                {
+                    var order = new Order {Price = model.Price, CreatedDate = DateTime.Now, Customer = customer};
+                    return order;
+                }
+
+                return null;
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex);
+                return null;
+            }
         }
 
-        // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
+        // POST V1/Customers/
+        [HttpPost]
+        public Customer Post(CustomerViewModel model)
         {
+            try
+            {
+                var orderAppDbContext = new OrdersAppDbContext();
+
+                var customer = new Customer {Email = model.Email, Name = model.Name};
+                orderAppDbContext.Customer.Add(customer);
+                orderAppDbContext.SaveChanges();
+
+                return customer;
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex);
+                return null;
+            }
         }
 
-        // DELETE api/values/5
-        public void Delete(int id)
-        {
-        }
     }
 }
